4bbdef4358d644258cd94ef1d6837c97c066800b
qq <qq@zhangwei-PC>
2013-05-12 15:44:25 +0800

    add msg4

--- jpush_server/src/cn/jpush/example/JpushClientExample.java
+++ jpush_server/src/cn/jpush/example/JpushClientExample.java
@@ -53,37 +53,76 @@ public class JpushClientExample {
 
 		//测试发送消息或者通知
 		//testSend();
-		sendMsg();
+		//sendMsg();
+		sendMsg4();
 		//testGson();
 	}
 
-	private static void testGson(){
+/*	private static void testGson(){
 		PushMessage pMsg = new PushMessage();
-		pMsg.bitmap_url = "http://1234/56we/kdji.jpg";
-		pMsg.type = 1;
+		pMsg.bitmap_url = "http://img1.gtimg.com/ent/pics/hv1/211/200/1321/85949236.jpg";
+		pMsg.type = 0;
 		pMsg.info = "本周来\'了'一批\\新的\"电影，哈哈";
 		pMsg.title = "自定义消息";
 		pMsg.time = System.currentTimeMillis();
 		
 		Gson gson = new Gson();
 		System.out.print(gson.toJson(pMsg));
+	}*/
+	
+	private static void sendMsg4(){
+		MessageManager.getInstance().notify.sendnotify = true;
+		MessageManager.getInstance().notify.info = "预置的影片1将于某日某时下线";
+		MessageManager.getInstance().notify.status_info = "预置的影片1将于某日某时下线，请尽快观看";
+		MessageManager.getInstance().notify.title = "预置的影片1";
+		
+		MessageManager.getInstance().condition.type = 1;
+		MessageManager.getInstance().condition.assetID = "123111";
+		MessageManager.getInstance().condition.videoname = "预置的影片1";
+		
+		PushMessage pMsg = new PushMessage();
+		pMsg.bitmap_url = "http://img1.gtimg.com/sports/pics/hv1/125/235/1323/86088125.jpg";
+		pMsg.type = 0;
+		pMsg.info = "预置的影片1将于某日某时下线，请您尽快观赏";
+		pMsg.title = "222陈晓做客";
+		pMsg.time = System.currentTimeMillis();
+		
+		MessageManager.getInstance().addMessage(pMsg);
+		
+		Gson gson = new Gson();
+		String msg_context = gson.toJson(MessageManager.getInstance()).replace("\"", "##");
+		System.out.println("客户端发送数据: " + msg_context);
+		MessageResult msgResult = jpush.sendCustomMessageWithAppKey(104, "custorm msg2", msg_context);//
+
+
+		
+		if (null != msgResult) {
+			System.out.println("服务器返回数据: " + msgResult.toString());
+			if (msgResult.getErrcode() == ErrorCodeEnum.NOERROR.value()) {
+				System.out.println("发送成功， sendNo=" + msgResult.getSendno());
+			} else {
+				System.out.println("发送失败， 错误代码=" + msgResult.getErrcode() + ", 错误消息=" + msgResult.getErrmsg());
+			}
+		} else {
+			System.out.println("无法获取数据，检查网络是否超时");
+		}
 	}
 	
 	private static void sendMsg(){
 		MessageManager.getInstance().notify.sendnotify = true;
-		MessageManager.getInstance().notify.info = "info";
-		MessageManager.getInstance().notify.status_info = "status_info";
-		MessageManager.getInstance().notify.title = "title";
+		MessageManager.getInstance().notify.info = "222畅聊《陆贞传奇》虐心的两段情";
+		MessageManager.getInstance().notify.status_info = "222畅聊《陆贞传奇》";
+		MessageManager.getInstance().notify.title = "222陈晓做客";
 		
 		MessageManager.getInstance().condition.type = 1;
 		MessageManager.getInstance().condition.assetID = "123456";
 		MessageManager.getInstance().condition.videoname = "video_name";
 		
 		PushMessage pMsg = new PushMessage();
-		pMsg.bitmap_url = "http://os.blog.163.com/common/ava.s?host=zhuowh2006@126&b=0&r=1291718794062";
-		pMsg.type = 1;
-		pMsg.info = "本周来了一批新的电影，哈哈";
-		pMsg.title = "自定义消息";
+		pMsg.bitmap_url = "http://img1.gtimg.com/sports/pics/hv1/124/235/1323/86088124.jpg";
+		pMsg.type = 0;
+		pMsg.info = "222畅聊《陆贞传奇》虐心的两段情，自曝想整容...[详细]";
+		pMsg.title = "222陈晓做客";
 		pMsg.time = System.currentTimeMillis();
 		
 		MessageManager.getInstance().addMessage(pMsg);
